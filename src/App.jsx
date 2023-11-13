0f521c235dcc1fd8c3c6148c83656a7ea6a804bc
Bateyjosue <josuebatey19@gmail.com>
2023-11-13 09:02:42 +0200

    feat: configure routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import Header from "./components/Header"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import MainLayout from "./components/layout/MainLayout"
+import ErrorPage from "./components/Error/ErrorPage"
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <MainLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        
+      ]
+    },
+  ])
+
   return (
     <>
-      <Header/>
+      <RouterProvider router={router} />
     </>
   )
 }
