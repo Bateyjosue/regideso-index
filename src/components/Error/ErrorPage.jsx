88570ca0d30252af95f1c2e4ea8e1a16e32afa6e
Bateyjosue <josuebatey19@gmail.com>
2023-11-13 08:59:42 +0200

    feat: add error page

---
+++ src/components/Error/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { Link, useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page" className="bg-blue-500 h-screen flex flex-col justify-center items-center gap-8 text-white">
+      <h1 className="text-6xl ">Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <Link to="/" className="border px-6 py-2 rounded-lg font-bold" >Go back</Link>
+    </div>
+  );
+}
