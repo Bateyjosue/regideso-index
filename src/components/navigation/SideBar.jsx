dec83a15f367070a5f2fd0120a0616b038c418fe
Bateyjosue <josuebatey19@gmail.com>
2023-11-15 07:48:48 +0200

    style: design the sidebar component

--- src/components/navigation/SideBar.jsx
+++ src/components/navigation/SideBar.jsx
@@ -2,30 +2,33 @@
 'use client';
 
 import { Sidebar } from 'flowbite-react';
-import { HiArrowSmRight, HiChartPie, HiShoppingBag, HiTable, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiChartPie, HiUser, HiDocumentReport, HiBell } from 'react-icons/hi';
 
 function SideBar() {
   return (
-    <Sidebar aria-label="Sidebar with logo branding example" className='-p-2'>
-      <Sidebar.Logo href="#" img="" imgAlt="">
-        Regideso
-      </Sidebar.Logo>
-      <Sidebar.Items className='-m-2 my-4'>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
+    <Sidebar aria-label="Sidebar with logo branding example" className='-p-2 flex flex-col '>
+      <Sidebar.Logo href="#" img="/src/assets/logo.png" imgAlt="" className="flex justify-center w-full h-1/5" />
+      <Sidebar.Items className='my-4 flex flex-col h-4/5'>
+        <Sidebar.ItemGroup className='gap-2 h-2/6'>
+          <Sidebar.Item href="#" icon={HiChartPie} className="py-2 mb-8 flex gap-2 justify-start px-12">
             Dashboard
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser}>
-            Users
+          <Sidebar.Item href="#" icon={HiUser} className="gap-4 py-2 justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
+            Agents
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag}>
-            Products
+          <Sidebar.Item href="#" icon={HiUser} className="gap-4 py-2 justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
+            Clients
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArrowSmRight}>
-            Sign In
+          <Sidebar.Item href="#" icon={HiBell} className="gap-4 py-2 justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
+            Fuites
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable}>
-            Sign Up
+          <Sidebar.Item href="#" icon={HiDocumentReport} className="gap-4 py-2  justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
+            Rapports
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className='gap-2 grow h-2/5 flex items-end justify-center pb-20'>
+          <Sidebar.Item href="/login" icon={HiArrowSmRight} className="gap-4 py-2 bg-blue-400 text-white font-bold px-4">
+            Logout
           </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
