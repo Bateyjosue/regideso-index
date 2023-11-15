6b50a4d3221ed7a2426bb210028a0d09f8eee624
Bateyjosue <josuebatey19@gmail.com>
2023-11-15 07:49:37 +0200

    feat: add main layout component

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -4,10 +4,10 @@ import SideBar from "../Navigation/SideBar"
 function MainLayout() {
   return (
     <div className="flex h-screen">
-      <header className="w-3/12">
+      <header className="w-3/12 bg-gray-300/20">
         <SideBar/>
       </header>
-      <main className="w-11/12 bg-green-400">
+      <main className="w-11/12">
         <Outlet className="w-full"/>
       </main>
     </div>
