6547ec72064671c75f019dccda161152a473a46f
Bateyjosue <josuebatey19@gmail.com>
2023-11-15 07:53:59 +0200

    feat: add routes for login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import MainLayout from "./components/layout/MainLayout"
 import ErrorPage from "./components/Error/ErrorPage"
+import LoginPage from "./components/auth/LoginPage"
+import RegisterPage from "./components/auth/RegisterPage"
 
 function App() {
 
@@ -10,13 +12,20 @@ function App() {
       element: <MainLayout />,
       errorElement: <ErrorPage />,
       children: [
-        
+        {
+          path: 'dashboard',
+          // element: <RegisterPage />
+        },
       ]
     },
     {
-      path: '/dashboard',
-      // element: <RegisterPage />
+      path: '/register',
+      element: <RegisterPage />
     },
+    {
+      path: '/login',
+      element: <LoginPage />
+    }
   ])
 
   return (
