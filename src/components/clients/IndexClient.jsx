b8aa17f0bc900a01726368710dc62f47edec84f1
Bateyjosue <josuebatey19@gmail.com>
2023-11-19 01:39:45 +0200

    feat: fetch index and create data loaders

--- src/components/clients/IndexClient.jsx
+++ src/components/clients/IndexClient.jsx
@@ -1,124 +1,85 @@
 import {useRef} from 'react';
 import { useDownloadExcel } from 'react-export-table-to-excel';
+import { useLoaderData} from 'react-router-dom'
 
+const baseUrl = 'https://regi-api.bingwainnovationhub.com/v1/'
+    
+
+export async function  loader() {
+    const subscribe = await fetch(`${baseUrl}subscribers?params={"page":"1", "limit":"10"}`)
+    const subscribeData = await subscribe.json()
+
+    const indexResponse = await fetch(`${baseUrl}index-samples?params={%22page%22:%221%22,%20%22limit%22:%22100%22}`)
+
+    const indexData = await indexResponse.json()
+
+    const subIndex = indexData.indexSamplesResponse.rows.map((index) => {
+        const subInd = subscribeData.subscribersResponse.rows.find(sub => sub.id === index.subscriber_id)
+        return {
+            ...subInd,
+            ...index
+        }
+    })
+    return subIndex
+}
 
 function IndexClient() {
-  const tableRef = useRef(null)
+    const tableRef = useRef(null)
+    const indexInfo =  useLoaderData()
 
   const { onDownload } = useDownloadExcel({
       currentTableRef: tableRef.current,
       filename: 'Users table',
       sheet: 'Users'
   })
-
+    
+    console.log(indexInfo);
+    
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-12 ">
-      <button onClick={onDownload} className='my-12 bg-green-600 px-4 text-white rounded-full'> Export excel </button>
-      <table ref={tableRef} className="w-full text-sm text-left rtl:text-right text-gray-500">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
-        <tr>
-        <th scope="col" className="p-4">
-        <div className="flex items-center">
-        <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2" />
-        <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
-        </div>
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Product name
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Color
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Category
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Accessories
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Available
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Price
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Weight
-        </th>
-        <th scope="col" className="px-6 py-3">
-        Action
-        </th>
-        </tr>
-        </thead>
-        <tbody>
-            <tr className="border-b dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td className="w-4 p-4">
-                    <div className="flex items-center">
-                        <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-                    </div>
-                </td>
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowra">
-                    Apple MacBook Pro 17
-                </th>
-                <td className="px-6 py-4">
-                    Silver
-                </td>
-                <td className="px-6 py-4">
-                    Laptop
-                </td>
-                <td className="px-6 py-4">
-                    Yes
-                </td>
-                <td className="px-6 py-4">
-                    Yes
-                </td>
-                <td className="px-6 py-4">
-                    $2999
-                </td>
-                <td className="px-6 py-4">
-                    3.0 lb.
-                </td>
-                <td className="flex items-center px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <a href="#" className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Remove</a>
-                </td>
-            </tr>
-            <tr className="bg-white border-b  dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td className="w-4 p-4">
-                    <div className="flex items-center">
-                        <input id="checkbox-table-search-2" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        <label htmlFor="checkbox-table-search-2" className="sr-only">checkbox</label>
-                    </div>
-                </td>
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    Microsoft Surface Pro
-                </th>
-                <td className="px-6 py-4">
-                    White
-                </td>
-                <td className="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td className="px-6 py-4">
-                    No
-                </td>
-                <td className="px-6 py-4">
-                    Yes
-                </td>
-                <td className="px-6 py-4">
-                    $1999
-                </td>
-                <td className="px-6 py-4">
-                    1.0 lb.
-                </td>
-                <td className="flex items-center px-6 py-4">
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <a href="#" className="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">Remove</a>
-                </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      <><button onClick={onDownload} className='my-12 bg-green-600 px-4 text-white rounded-full'> Export excel </button><div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-12 ">
+          <table ref={tableRef} className="w-full text-sm text-left rtl:text-right text-gray-500">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+                  <tr>
+                      <th scope="col" className="px-6 py-3">
+                          Index Number
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          Abonne
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          M3 Consumed
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          Pour le Mois de?
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+                { 
+                  indexInfo ? (
+                    indexInfo.map(index => (
+                      <tr
+                        key={index.id}
+                        className="border-b dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowra">
+                            {index.id}
+                        </td>
+                        <td className="px-6 py-4">
+                            {index.full_name}
+                        </td>
+                        <td className="px-6 py-4">
+                            {index.consumed_water}
+                        </td>
+                        <td className="px-6 py-4">
+                            {index.updated_at}
+                        </td>
+                      </tr>
+                    ))
+                  ) : (<tr>No Index Found</tr>)
+                }
+              </tbody>
+          </table>
+      </div></>
   )
 }
 export default IndexClient
