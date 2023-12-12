f9d9c9660e47bd51eff50c98b6b5eb53f4b9b6a4
Bateyjosue <josuebatey19@gmail.com>
2023-12-12 08:59:36 +0200

    feat: style navbar

--- src/components/navigation/SideBar.jsx
+++ src/components/navigation/SideBar.jsx
@@ -16,33 +16,37 @@ function SideBar() {
         img={IMAGES.regideso}
         imgAlt=""
         className="w-full h-1/4" />
-      <Sidebar.Items className='my-4 flex flex-col h-4/5'>
+      <Sidebar.Items className='my-4 flex flex-col h-4/5 '>
         <Sidebar.ItemGroup className='gap-2 h-2/6'>
-          <Sidebar.Item href="/" icon={HiChartPie} className="gap-4 py-2 justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
+          <Sidebar.Item href="/" icon={HiChartPie} className="gap-4 py-2 justify-start px-4 hover:bg-blue-200 hover:text-white hover:rounded-none">
             Dashboard
           </Sidebar.Item>
-          <Sidebar.Collapse icon={HiShoppingBag} label="Agents" className="gap-4 py-2 justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
-            <Sidebar.Item href="#">List des agents</Sidebar.Item>
-            <Sidebar.Item href="#">Ajouter un Agent</Sidebar.Item>
+          <Sidebar.Collapse icon={HiShoppingBag} label="Agents" className="gap-4 py-2 justify-start px-4 hover:bg-blue-200 hover:text-white hover:rounded-none">
+            <div className=' flex flex-col items-start pl-4'>
+              <Sidebar.Item href="#" className="hover:bg-blue-200 hover:text-white">List des agents</Sidebar.Item>
+              <Sidebar.Item href="#" className="hover:bg-blue-200 hover:text-white">Ajouter un Agent</Sidebar.Item>
+            </div>
             <Sidebar.Item href="#"></Sidebar.Item>
           </Sidebar.Collapse>
-          <Sidebar.Collapse href="#" icon={HiUser} label="Abonnes" className="gap-4 py-2 justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
-            <Sidebar.Item>
-              <Link to='subscriber'>List des Abonnes</Link>
+          <Sidebar.Collapse href="#" icon={HiUser} label="Abonnes" className="gap-4 py-2 justify-start px-4 hover:bg-blue-200 hover:text-white hover:rounded-none ">
+            <div className=' flex flex-col items-start pl-4'>
+            <Sidebar.Item className="hover:bg-blue-200 hover:text-white">
+              <Link to='subscriber'>Tous les Abonnes</Link>
             </Sidebar.Item>
-            <Sidebar.Item>
-              <Link to='new-subscriber'>Ajouter un Abonne</Link>
+            <Sidebar.Item className="hover:bg-blue-200 hover:text-white">
+              <Link to='new-subscriber'>Add new Subscriber</Link>
             </Sidebar.Item>
+            </div>
           </Sidebar.Collapse>
-          <Sidebar.Item icon={HiBell} className="gap-4 py-2 justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
+          <Sidebar.Item icon={HiBell} className="gap-4 py-2 justify-start px-4 hover:bg-blue-200 hover:text-white hover:rounded-none">
             <Link to='index-client'>Index Clients</Link>
           </Sidebar.Item>
-          <Sidebar.Item icon={HiBell} className="gap-4 py-2 justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
+          <Sidebar.Item icon={HiBell} className="gap-4 py-2 justify-start px-4 hover:bg-blue-200 hover:text-white hover:rounded-none">
             <Link to='fuites'>
               Fuites
             </Link>
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiDocumentReport} className="gap-4 py-2  justify-start px-12 hover:bg-blue-200 hover:text-white hover:rounded-none">
+          <Sidebar.Item href="#" icon={HiDocumentReport} className="gap-4 py-2  justify-start px-4 hover:bg-blue-200 hover:text-white hover:rounded-none">
             Rapports
           </Sidebar.Item>
         </Sidebar.ItemGroup>
