37eb8989f00f19cb887a71d562dce7a296710608
Josue Batey <josuebatey19@gmail.com>
2025-06-19 19:16:02 +0200

    Updated Login.tsx

--- src/components/pages/auth/Login.tsx
+++ src/components/pages/auth/Login.tsx
@@ -35,7 +35,7 @@ export default function LoginPage() {
           <div className="lg:w-1/2 bg-gradient-to-br from-blue-600 via-blue-700 to-blue-800 p-8 lg:p-12 flex flex-col justify-center items-center text-white relative overflow-hidden">
             {/* Background Pattern */}
             <div className="absolute inset-0 opacity-10">
-              <div className="absolute top-0 left-0 w-full h-full bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%23ffffff\" fill-opacity=\"0.1\"%3E%3Ccircle cx=\"30\" cy=\"30\" r=\"4\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"></div>
+              <div className="absolute top-0 left-0 w-full h-full "></div>
             </div>
             
             <div className="relative z-10 text-center">
