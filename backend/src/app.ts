11ea2e317f61b24a09cede72509c9ec24de78072
Bateyjosue <josuebatey19@gmail.com>
2025-12-02 17:48:29 +0200

    feat(backend): add morgan and rate limit middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,14 +2,27 @@ import express, { type Express } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
+import morgan from 'morgan';
+import ratelimit from 'express-rate-limit';
 
 const app: Express = express();
 
+const limiter = ratelimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(compression());
+
+app.use(morgan('dev'));
 
 // routes
 app.get('/', (req, res) => {
